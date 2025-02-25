7879bb891da4cfdf2798539ed00c96f0e677bfd4
Emmanuelekpeh <eekpehsamuel@gmail.com>
2025-02-25 10:16:23 -0500

    Update connection.ts

--- server/db/connection.ts
+++ server/db/connection.ts
@@ -1,6 +1,8 @@
 import { Pool } from '@neondatabase/serverless';
 import { drizzle } from 'drizzle-orm/neon-serverless';
 import config from '../../config';
+// Add logger import
+import { logger } from '../utils/logger';
 
 if (!process.env.DATABASE_URL) {
   throw new Error("DATABASE_URL must be set");
@@ -14,8 +16,13 @@ const pool = new Pool({
   connectionTimeoutMillis: config.database.connectionTimeoutMillis
 });
 
+// Add these event listeners after creating the pool
+pool.on('connect', () => {
+  console.info('New database connection established');
+});
+
 pool.on('error', (err) => {
-  console.error('Unexpected error on idle client', err);
+  logger.error('Unexpected error on idle client', err);
   process.exit(-1);
 });
 
