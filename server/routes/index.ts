ce75d5a6bb6af20f112e2a69be3f35e06ff04f36
Emmanuelekpeh <eekpehsamuel@gmail.com>
2025-02-27 09:05:25 -0500

    Create index.ts

---
+++ server/routes/index.ts
@@ -0,0 +1,10 @@
+import { Express } from 'express';
+import userRoutes from './user.routes';
+import puzzleRoutes from './puzzle.routes';
+import lichessRoutes from './lichess.routes';
+
+export function registerRoutes(app: Express) {
+  app.use('/api/users', userRoutes);
+  app.use('/api/puzzles', puzzleRoutes);
+  app.use('/api/lichess', lichessRoutes);
+}
