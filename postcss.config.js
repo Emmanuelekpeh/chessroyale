6c910679712f60464835764c0b6009a685f68891
Emmanuelekpeh <eekpehsamuel@gmail.com>
2025-02-27 10:55:24 -0500

    Update postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,8 @@
-export default {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
+module.exports = {
+  plugins: [
+    require('postcss-import'),
+    require('tailwindcss/nesting'),
+    require('tailwindcss'),
+    require('autoprefixer'),
+  ]
 }
