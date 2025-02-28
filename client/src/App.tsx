693325e2fd05a965a6598e2ec6ae7eef6055d9eb
Emmanuelekpeh <eekpehsamuel@gmail.com>
2025-02-27 20:13:20 -0500

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,13 @@
 import { Switch, Route } from "wouter";
-import { queryClient } from "./lib/queryClient";
+import { queryClient } from "@/lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
 import NotFound from "@/pages/not-found";
 import AuthPage from "@/pages/auth-page";
 import HomePage from "@/pages/home-page";
 import GameRoom from "@/pages/game-room";
-import { AuthProvider } from "./hooks/use-auth";
-import { ProtectedRoute } from "./lib/protected-route";
+import { AuthProvider } from "@/hooks/use-auth";
+import { ProtectedRoute } from "@/lib/protected-route";
 
 function Router() {
   return (
