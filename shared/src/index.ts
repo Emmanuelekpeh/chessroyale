26bda60e1a3b133bf2fa42e4a44e7530be5223a9
Emmanuelekpeh <eekpehsamuel@gmail.com>
2025-02-28 13:19:31 -0500

    Update index.ts

--- shared/src/index.ts
+++ shared/src/index.ts
@@ -1,6 +1,5 @@
 // Re-export all types
 export type { User, Puzzle, Achievement } from './types';
 export type { GameState, ChatMessage } from '../types/game';
-
 // Export everything from schema
 export * from './schema';
