3336e3eae8ee16de75692bf02fda05081879289d
Emmanuelekpeh <eekpehsamuel@gmail.com>
2025-03-01 02:13:03 -0500

    Update db.ts

--- server/src/db.ts
+++ server/src/db.ts
@@ -1,7 +1,7 @@
 import { Pool, neonConfig } from '@neondatabase/serverless';
 import { drizzle } from 'drizzle-orm/neon-serverless';
 import ws from "ws";
-import * as schema from "@shared/schemas";
+import * as schema from "@shared/schema"; // Changed from schemas to schema
 
 neonConfig.webSocketConstructor = ws;
 
